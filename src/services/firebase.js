8b818cefd869f4081004f99cf768909aef6ab111
atharva0712 <atharvast0712@gmail.com>
2025-07-07 06:24:24 +0530

    Cleared some security vulnerabilities

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,19 +1,14 @@
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA1kB7ylewuwlf0vYeuGUI5jrpJ-532Xug",
-  authDomain: "timetablegenerator-ca71c.firebaseapp.com",
-  projectId: "timetablegenerator-ca71c",
-  storageBucket: "timetablegenerator-ca71c.firebasestorage.app",
-  messagingSenderId: "228834738888",
-  appId: "1:228834738888:web:1bc79a77ba7f75bc631bfd"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
